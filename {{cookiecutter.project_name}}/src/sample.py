edd28a206d183a984fa8beeeb077ca86b202503b
Tolga Ayan <tolgayan98@gmail.com>
2021-12-29 20:21:43 +0300

    Add poetry with sample code

---
+++ {{cookiecutter.project_name}}/src/sample.py
@@ -0,0 +1,14 @@
+
+
+def sum(a, b):
+    return a + b
+
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+def very_long_function():
+    aaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbcccccccccccccccccccccccccccccccc = 1
+    return 1
---
+++ {{cookiecutter.project_name}}/tests/test_sample.py
@@ -0,0 +1,20 @@
+
+
+from src.sample import sum, fibonacci
+
+def test_sum():
+    assert sum(1, 2) == 3
+    assert sum(2, 2) == 4
+
+def test_fibonacci():
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(7) == 13
+    assert fibonacci(8) == 21
+    assert fibonacci(9) == 34
+    assert fibonacci(10) == 55
